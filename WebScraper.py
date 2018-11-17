65acb9eee177bd66886d30e99bd4ba84f21b3564
Ken <kenkilmartin@gmail.com>
2018-11-17 00:38:48 +0000

    added all counties and did a scrape of the first 20 of all which resulted in 520 entries

--- WebScraper.py
+++ WebScraper.py
@@ -6,9 +6,9 @@ import datetime
 MAX_PAGE_NUM = 1
 
 #need to download the chromedriver to get it to open page
-#chrome_path = r'C:\Python\Python37\Scripts\chromedriver\chromedriver.exe'
+chrome_path = r'C:\Python\Python37\Scripts\chromedriver\chromedriver.exe'
 #The below path is for laptop
-chrome_path = r'C:\Python\Python37-32\Scripts\chromedriver.exe'
+#chrome_path = r'C:\Python\Python37-32\Scripts\chromedriver.exe'
 driver = webdriver.Chrome(chrome_path)
 
 now = datetime.datetime.now()
@@ -31,7 +31,6 @@ def getDataByCounty():
             url = "https://www.daft.ie/"+line+"/apartments-for-rent/?s%5Bignored_agents%5D%5B0%5D=428&s%5Bignored_agents%5D%5B1%5D=1551&offset=" + str(
                 listings)
             driver.get(url)
-            # "https://www.daft.ie/dublin-city/apartments-for-rent/?s%5Bignored_agents%5D%5B0%5D=428&s%5Bignored_agents%5D%5B1%5D=1551&offset="
             houses = driver.find_elements_by_class_name("box")
             crawlycrawl(houses)
         print(line)
@@ -50,6 +49,7 @@ going through each house on each page and
 def crawlycrawl(houses):
     for house in houses:
         houseName = house.find_element_by_class_name("search_result_title_box").text
+        # and '- House to Ren'
         startOfStringToRemove = houseName.find(' - Apartment to Rent')
         houseName = houseName[:startOfStringToRemove]
         price = house.find_element_by_class_name("info-box").find_element_by_class_name("price")
@@ -58,10 +58,10 @@ def crawlycrawl(houses):
        # print(houseName.text + " " + price.text)
         priceText = ''
 
-        """""
+        """
         This is looking for the price and if the price is per week if it finds per week it multiplys by 4 else it saves the value
         it also takes out the euro symbol and the per month per week string
-        """""
+        """
 
         for i in range(0, len(price.text)):
             if str.isdigit(price.text[i]):
